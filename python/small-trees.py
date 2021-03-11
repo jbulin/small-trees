9f692bc1625b1868c3393cd6f74eab1ad0748567
jb <20406010+jbulin@users.noreply.github.com>
2021-03-11 18:37:39 +0100

    Generate trees and triads

---
+++ python/small-trees.py
@@ -0,0 +1,24 @@
+# Computing polymorphisms of small trees
+
+import datetime
+import os
+
+
+def generate_trees(n):
+    # generate oriented trees of size {n} using nauty
+    command = f"gentreeg -q {n} | watercluster2 S T"
+    os.system(command)
+
+
+def generate_triads(n):
+    # generate oriented triads of size {n} using nauty
+    command = f"gentreeg -q -D3 {n} | pickg -D3 -M3 | watercluster2 S T"
+    os.system(command)
+
+
+def main():
+    for n in range(1, 10):
+        generate_trees(n)
+
+
+main()
