14a8455e96d927304853963660a93e30ccd80dbe
jb <20406010+jbulin@users.noreply.github.com>
2021-03-11 18:44:37 +0100

    Edit readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # small-trees
- Computing polymorphisms of small oriented trees.
+Computing polymorphisms of small oriented trees.
+
+Trees are generated using nauty. Tested with version 2.7 (r1).
--- python/small-trees.py
+++ python/small-trees.py
@@ -7,7 +7,7 @@ import os
 def generate_trees(n):
     # generate oriented trees of size {n} using nauty
     command = f"gentreeg -q {n} | watercluster2 S T"
-    os.system(command)
+    print(os.system(command))
 
 
 def generate_triads(n):
